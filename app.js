eb2ebce644c12ca9e0f18e2646d11266001407a3
Menik <menikdisney@gmail.com>
2019-05-21 10:35:55 +0600

    Using response object in createService method make ersponse header, add html content

--- app.js
+++ app.js
@@ -21,9 +21,52 @@ http.createServer(function (req, res) {
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type','text/html');
+    res.write(`
+    
+    <!DOCTYPE html>
+     <html lang="en">
 
-    /*Quiting from event loop*/
-    process.exit();
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Material Design Bootstrap</title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+  <!-- Bootstrap core CSS -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.1/css/mdb.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- Start your project here-->
+  <div style="height: 100vh">
+    <p>Node Server creating Response</p>
+  </div>
+  <!-- /Start your project here-->
+
+  <!-- JQuery -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.1/js/mdb.min.js"></script>
+</body>
+
+</html>
+    `);
+
+    res.end();
+
+
+
+    /*Quiting from event loop
+    process.exit();*/
 });
 
 server.listen(3000);
