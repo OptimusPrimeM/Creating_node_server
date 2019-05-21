da6f6b71f0c93fb29df9608dfe66ac02403208bb
Menik <menikdisney@gmail.com>
2019-05-21 09:58:05 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+/* Define seperate function and add that to createServer()
+
+function reqListner(req, res) {
+    console.log(req);
+}
+
+http.createServer(reqListner);
+*/
+
+/* Define anonymous function and add that to createServer()
+
+http.createServer(function (req, res) {
+    console.log(req);
+});
+
+*/
+
+/* Using arrow function*/
+
+const server = http.createServer((req, res) => {
+    console.log(req);
+
+    /*Quiting from event loop*/
+    process.exit();
+});
+
+server.listen(3000);
