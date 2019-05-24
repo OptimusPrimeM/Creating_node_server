2f21864e5ef5b343bcaa4834e8124508f2c1f92a
Menik <menikdisney@gmail.com>
2019-05-24 12:59:39 +0530

    Start to use express.js framework

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+/*Code modulse*/
 const http = require('http');
+
+/*Code modules*/
+const express = require('express');
+
+/*Custom modules*/
 const routes = require('./routes');
+
+const app = express();
 
 const server = http.createServer(routes);
 
