3b14f140133390eca24e25220d92b0a9e536cd2c
Menik <menikdisney@gmail.com>
2019-05-27 11:15:02 +0530

    Routing using app.use() first path parameter

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    next();
+app.use('/add-product', (req, res, next) => {
+    res.send(`<h1>Add products</h1>`);
 });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     res.send(`<h1>Hello from express()</h1>`);
 });
 
